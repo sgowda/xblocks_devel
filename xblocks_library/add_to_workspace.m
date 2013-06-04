477caeea9fbdaa4929dbbebe62d50ee00fd27b20
Suraj Gowda <sgowda@stitches.jpl.nasa.gov>
2013-06-04 14:53:49 -0700

    xBlocks for FromWorkspace, ToWorkspace (for testbenches)

--- xblocks_library/add_to_workspace.m
+++ xblocks_library/add_to_workspace.m
@@ -3,4 +3,5 @@ function [] = add_to_workspace(sys, var_name, varargin)
     config.depend = {'mat_output_data_init_xblock.m'};
     config.toplevel = sprintf('%s/%s', sys, var_name);
     xBlock(config, {config.toplevel, 'var_name', var_name, varargin{:}});   
+    set_param(config.toplevel, 'ShowName', 'off')
 end
