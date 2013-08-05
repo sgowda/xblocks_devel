80ac81eeaa492d74794ed9c4cf4c5d3c609c8455
Suraj Gowda <sgowda@vostro.jpl.nasa.gov>
2013-08-05 11:19:59 -0700

    adding generator function 'const' and use case in kurtosis_data_serializer

---
+++ xblocks_library/const.m
@@ -0,0 +1,9 @@
+function [val] = const(name, fl_val, dtype)
+
+config.source = 'Constant';
+config.name = name;
+val = xSignal();
+xBlock(config, ...
+    {'const', fl_val, 'arith_type', arith_type(dtype), 'n_bits', ...
+        n_bits(dtype), 'bin_pt', bin_pt(dtype), 'explicit_period', 'on'}, ...
+    {}, {val});
