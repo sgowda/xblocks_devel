b0d83573f5e181442aa62d7cc3038e5167e715bd
Suraj Gowda <sgowda@thundercat.jpl.nasa.gov>
2013-08-16 08:59:51 -0700

    added 'Position' parameter to make the generated block size smaller

--- xblocks_library/xblock_delay.m
+++ xblocks_library/xblock_delay.m
@@ -8,8 +8,8 @@ end
 
 for m = 1:M
     for n =1:N
-	    xBlock( struct('source', 'Delay', 'name', [name, '_delay_', num2str(m), '_', num2str(n)]), ...
-	    	struct('latency', latency), {din{m,n}}, {dout{m,n}});
+	    xBlock( struct('source', 'Delay', 'name', sprintf('%s_delay_%d_%d', name, m, n)), ...
+	    	struct('latency', latency, 'Position',  [330   964   370   986],  'ShowName', 'off'), din(m,n), dout(m,n));
     end
 end
 
