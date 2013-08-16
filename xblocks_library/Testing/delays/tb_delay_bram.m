4430224cd882b406f861d945e466c7eb1a56ee3e
Suraj Gowda <sgowda@thundercat.jpl.nasa.gov>
2013-08-16 08:31:07 -0700

    minor changes to delay bram testbench

--- xblocks_library/Testing/delays/tb_delay_bram.m
+++ xblocks_library/Testing/delays/tb_delay_bram.m
@@ -3,26 +3,34 @@ clc; clear; close all
 mdl_name = 'delay_bram';
 eval(mdl_name); % open design
 
-%% Re-draw blocks
-latency = 10;
-config.source = @delay_bram_init_xblock;
-config.toplevel = subblockname(mdl_name, 'delay_bram');
-xBlock(config, {config.toplevel, 'latency', latency});
+%% Test parameters to sweep over
+latencies = [5, 6, 7, 8];
+bram_latencies = [2, 3, 4];
 
 %% Generate simulation inputs
 T_sim = 100;
-[din, din_data] = data_burst_real(T_sim - latency - 5, T_sim, 1);
-din_data_fi = double(fi(din_data, 1, 18, 17)); % TODO use actual rounding
+data_len = T_sim - max(latencies) - max(bram_latencies) - 1;
+[din, din_data] = data_burst_real(data_len, T_sim, 1);
+din_data_fi = double(fi(din_data, 1, 18, 17)); % TODO use 'quantize' function
 din = construct_sim_timeseries(din_data_fi, T_sim);
 
-%% Simulate
-% Set simulation time
-set_param(mdl_name, 'StopTime', num2str(T_sim-1));
+%% Re-draw blocks
+for latency=latencies
+    for bram_latency=bram_latencies
+        config.source = @delay_bram_init_xblock;
+        config.toplevel = subblockname(mdl_name, 'delay_bram');
+        xBlock(config, {config.toplevel, 'latency', latency, 'bram_latency', bram_latency});
+        
+        %-- Simulate
+        % Set simulation time
+        set_param(mdl_name, 'StopTime', num2str(T_sim-1));
 
-% Run simulation
-sim(mdl_name)
+        % Run simulation
+        sim(mdl_name)
 
-%% Verify
-start = 1;
-x = [0, din_data_fi]';
-all(x == dout(latency+start : latency+start+length(din_data)))
+        %-- Verify
+        start = 1;
+        x = [0, din_data_fi]';
+        all(x == dout(latency+start : latency+start+length(din_data)))        
+    end
+end
