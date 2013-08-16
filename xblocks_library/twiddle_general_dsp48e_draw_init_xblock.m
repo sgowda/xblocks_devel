b73c35cad6a6018774826d8327372ae7bcc57b7e
Suraj Gowda <sgowda@thundercat.jpl.nasa.gov>
2013-08-16 08:57:54 -0700

    altered to reflect the changes to FFT to move the coefficient generators outside of the butterfly block

--- xblocks_library/twiddle_general_dsp48e_draw_init_xblock.m
+++ xblocks_library/twiddle_general_dsp48e_draw_init_xblock.m
@@ -1,7 +1,7 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %                                                                             %
 %   Center for Astronomy Signal Processing and Electronics Research           %
-%   http://casper.berkeley.edu                                                %      
+%   http://casper.berkeley.edu                                                %
 %   Copyright (C) 2011 Suraj Gowda    Hong Chen                               %
 %                                                                             %
 %   This program is free software; you can redistribute it and/or modify      %
@@ -19,7 +19,7 @@
 %   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.               %
 %                                                                             %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-function twiddle_general_dsp48e_draw_init_xblock(a_re, a_im, b_re, b_im, sync, ...
+function twiddle_general_dsp48e_draw_init_xblock(a_re, a_im, b_re, b_im, w_re, w_im, sync, ...
     a_re_out, a_im_out, bw_re_out, bw_im_out, sync_out, ...
     Coeffs, StepPeriod, coeff_bit_width, input_bit_width, bram_latency,...
     conv_latency, quantization, overflow, arch, coeffs_bram, FFTSize)
@@ -30,54 +30,52 @@ function twiddle_general_dsp48e_draw_init_xblock(a_re, a_im, b_re, b_im, sync, .
 
 %% diagram
 % parameters
-total_latency = bram_latency + 4 + conv_latency;
+cmult_dsp48e_latency = 4; 
+total_latency = cmult_dsp48e_latency + conv_latency;
+% total_latency = bram_latency + 4 + conv_latency;
 
 % signals
-w = xSignal;
-w_re = xSignal;
-w_im = xSignal;
+% w = xSignal;
+% w_re = xSignal;
+% w_im = xSignal;
 b_re_del = xSignal;
 b_im_del = xSignal;
 
-% delay sync by total_latency 
+% delay sync by total_latency
 sync_delay = xBlock(struct('source', 'Delay', 'name', 'sync_delay'), ...
-                       struct('latency', total_latency), ...
-                       {sync}, ...
-                       {sync_out});
+    struct('latency', total_latency), ...
+    {sync}, ...
+    {sync_out});
 
-% delay a_re by total latency 
+% delay a_re by total latency
 a_re_delay = xBlock(struct('source', 'Delay', 'name', 'a_re_delay'), ...
-                       struct('latency', total_latency, 'reg_retiming', 'on'), {a_re}, {a_re_out});
+    struct('latency', total_latency, 'reg_retiming', 'on'), {a_re}, {a_re_out});
 
-% delay a_im by total latency 
+% delay a_im by total latency
 a_im_delay = xBlock(struct('source', 'Delay', 'name', 'a_im_delay'), ...
-                       struct('latency', total_latency, 'reg_retiming', 'on'), {a_im}, {a_im_out});
+    struct('latency', total_latency, 'reg_retiming', 'on'), {a_im}, {a_im_out});
 
-% delay b_re by bram_latency 
-b_re_delay = xBlock(struct('source', 'Delay', 'name', 'b_re_delay'), ...
-                       struct('latency', bram_latency, 'reg_retiming', 'on'), {b_re}, {b_re_del});
+% % delay b_re by bram_latency
+% b_re_delay = xBlock(struct('source', 'Delay', 'name', 'b_re_delay'), ...
+%     struct('latency', bram_latency, 'reg_retiming', 'on'), {b_re}, {b_re_del});
+% 
+% % delay b_im by bram_latency
+% b_im_delay = xBlock(struct('source', 'Delay', 'name', 'b_im_delay'), ...
+%     struct('latency', bram_latency, 'reg_retiming', 'on'), {b_im}, {b_im_del});
 
-% delay b_im by bram_latency 
-b_im_delay = xBlock(struct('source', 'Delay', 'name', 'b_im_delay'), ...
-                       struct('latency', bram_latency, 'reg_retiming', 'on'), {b_im}, {b_im_del});
+% % convert 'w' to real/imag
+% c_to_ri_w = xBlock(struct('source', str2func('c_to_ri_init_xblock'), 'name', 'c_to_ri_w'), ...
+%     {[], coeff_bit_width, coeff_bit_width-2}, {w}, {w_re, w_im});
 
+xBlock(struct('source', @cmult_dsp48e_init_xblock, 'name', 'cmult'), ...
+    {[],input_bit_width, input_bit_width - 1, coeff_bit_width, coeff_bit_width - 2, 'off', ...
+    'off', input_bit_width + 4, input_bit_width + 1, quantization, ...
+    overflow, conv_latency}, ...
+    {b_re, b_im, w_re, w_im}, ...
+    {bw_re_out, bw_im_out});
 
-% convert 'w' to real/imag 
-c_to_ri_w = xBlock(struct('source', str2func('c_to_ri_init_xblock'), 'name', 'c_to_ri_w'), ...
-                               {[], coeff_bit_width, coeff_bit_width-2}, {w}, {w_re, w_im});
-
-                                        
-% block: twiddles_collections/twiddle_general_dsp48e/cmult
-cmult_sub = xBlock(struct('source', str2func('cmult_dsp48e_init_xblock'), 'name', 'cmult'), ...
-                      {[],input_bit_width, input_bit_width - 1, coeff_bit_width, coeff_bit_width - 2, 'off', ...
-                      	'off', input_bit_width + 4, input_bit_width + 1, quantization, ... 
-                      	overflow, conv_latency}, ...
-                      {b_re_del, b_im_del, w_re, w_im}, ...
-                      {bw_re_out, bw_im_out});
-
-       
 % instantiate coefficient generator
-coeff_gen_sub = xBlock(struct('source',str2func('coeff_gen_init_xblock'), 'name', 'coeff_gen'), ...
-                          {[], Coeffs, coeff_bit_width, StepPeriod, bram_latency, coeffs_bram}, {sync}, {w});
-                                 
+% coeff_gen_sub = xBlock(struct('source',str2func('coeff_gen_init_xblock'), 'name', 'coeff_gen'), ...
+%     {[], Coeffs, coeff_bit_width, StepPeriod, bram_latency, coeffs_bram}, {sync}, {w});
+
 end
