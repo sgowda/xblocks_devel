2e48d8a6a2169fe52b44d62f3bccacf2df640cb8
Suraj Gowda <sgowda@thundercat.jpl.nasa.gov>
2013-08-20 16:06:51 -0700

    hack-fixed Xilinx inconsistency with string datatypes (string for 2's comp datatype occasionally has extra spaces, but only for some blocks....)

--- xblocks_library/const.m
+++ xblocks_library/const.m
@@ -4,7 +4,14 @@ function [val] = const(name, fl_val, dtype, varargin)
 config.source = 'Constant';
 config.name = name;
 val = xSignal();
+
+if dtype.Signed == 1
+	arith_type = 'Signed (2''s comp)';
+else
+    arith_type = 'Unsigned';
+end    
+
 xBlock(config, ...
-    {'const', fl_val, 'arith_type', arith_type(dtype), 'n_bits', ...
+    {'const', fl_val, 'arith_type', arith_type, 'n_bits', ...
         n_bits(dtype), 'bin_pt', bin_pt(dtype), 'explicit_period', 'on'}, ...
     {}, {val});
