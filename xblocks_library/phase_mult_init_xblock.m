7956249a875aa058b0d2442466b8f6b0be2420ec
Suraj Gowda <sgowda@thundercat.jpl.nasa.gov>
2013-08-27 13:24:41 -0700

    added switch for dsp48e instatntiation; original (behav) test still passes

--- xblocks_library/phase_mult_init_xblock.m
+++ xblocks_library/phase_mult_init_xblock.m
@@ -3,7 +3,7 @@ function [] = phase_mult_init_xblock(blk, varargin)
 defaults = {'n_inputs', 4, 'biplex_fft_length', 32, 'read_latency', 2, 'use_bram', 1, ...
     'input_bit_width', 18, 'input_bin_pt', 17, 'coeff_bit_width', 18, 'coeff_bin_pt', 17, ...
     'mult_latency', 3, 'add_latency', 2, 'conv_latency', 1, 'quantization', 'Truncate', ...
-    'overflow', 'Wrap'};
+    'overflow', 'Wrap', 'cmult_impl', 'behav'};
 
 n_inputs = get_var('n_inputs', 'defaults', defaults, varargin{:});
 biplex_fft_length = get_var('biplex_fft_length', 'defaults', defaults, varargin{:});
@@ -18,9 +18,16 @@ input_bit_width = get_var('input_bit_width', 'defaults', defaults, varargin{:});
 input_bin_pt = get_var('input_bin_pt', 'defaults', defaults, varargin{:});
 quantization = get_var('quantization', 'defaults', defaults, varargin{:});
 overflow = get_var('overflow', 'defaults', defaults, varargin{:});
+cmult_impl = get_var('cmult_impl', 'defaults', defaults, varargin{:});
 
-cmult_latency = mult_latency + add_latency + conv_latency; % TODO fixed latency for DSP48E implementation
-
+if strcmp(cmult_impl, 'behav')
+    cmult_fn = @cmult_behav_init_xblock;
+    cmult_latency = mult_latency + add_latency + conv_latency;
+elseif strcmp(cmult_impl, 'dsp48e')
+    cmult_fn = @cmult_dsp48e_init_xblock;
+    cmult_latency = 4 + conv_latency; % fixed latency for DSP48E implementation
+end
+    
 sync = xInport('sync');
 sync_out = xOutport('sync_out');
 
@@ -32,7 +39,7 @@ for k=1:n_inputs
     phase_coeffs = exp(-1j*2*pi/(n_inputs*biplex_fft_length)*(0:biplex_fft_length-1)*(k-1));
     coeff_blkname = sprintf('phase%d_coeffs', k);
     xBlock(struct('name', coeff_blkname, 'source', str2func('coeff_gen_init_xblock')), ...
-        {sprintf('%s/%s', blk, coeff_blkname), phase_coeffs, coeff_bit_width, 0, read_latency, 'on'}, {sync}, {coeffs});
+        {sprintf('%s/%s', blk, coeff_blkname), 'Coeffs', phase_coeffs, 'coeff_bit_width', coeff_bit_width, 'StepPeriod', 0, 'bram_latency', read_latency, 'use_bram', 1}, {sync}, {coeffs});
 
     %---- delay data inputs by 'read_latency'
     din_del = xSignal();
@@ -42,22 +49,15 @@ for k=1:n_inputs
     % multipliers
     dout_name = sprintf('dout%d', k);
     dout = xOutport(dout_name);
-    xBlock(struct('source', 'cmult_behav_init_xblock', 'name', sprintf('cmult_%d', k)), ...
+    xBlock(struct('source', cmult_fn, 'name', sprintf('cmult_%d', k)), ...
         {sprintf('%s/cmult_%d', blk, k), 'n_bits_a', input_bit_width, 'bin_pt_a', input_bin_pt, 'n_bits_b', coeff_bit_width, 'bin_pt_b', coeff_bin_pt, 'conjugated', 0, ...
             'full_precision', 0, 'n_bits_c', input_bit_width, 'bin_pt_c', input_bin_pt, 'quantization', quantization, 'overflow', overflow, 'cplx_inputs', 1}, ...
-        {din_del, coeffs}, {dout});  
-    
-%     defaults = {'n_bits_a', 18, 'bin_pt_a', 17, 'n_bits_b', 18, 'bin_pt_b', 17, 'conjugated', 0, ...
-% 	'full_precision', 1, 'n_bits_c', 18, 'bin_pt_c', 17, 'quantization', 'Truncate', 'overflow', 'Wrap', ...
-%     'cplx_inputs', 0, 'mult_latency', 3, 'add_latency', 2, 'conv_latency', 1};
-    
-    
+        {din_del, coeffs}, {dout});
 end
 
-% TODO sync delay
+% sync delay
 sync_latency = read_latency + cmult_latency;
-
-xBlock(struct('source', 'Delay', 'name', 'sync_del'), ...
-    {'latency', sync_latency}, {sync}, {sync_out});
+sync_out_sig = delay_srl('sync_del', sync, sync_latency);
+sync_out.bind(sync_out_sig);
 
 end
