1172f17a5533917cef746cf17419f5682538b031
Suraj Gowda <sgowda@thundercat.jpl.nasa.gov>
2013-08-29 16:28:18 -0700

    added option for latency (used in vacc serialization)

--- xblocks_library/mux_select.m
+++ xblocks_library/mux_select.m
@@ -1,4 +1,6 @@
-function [selected] = mux_select(name, sel, inputs)
+function [selected] = mux_select(name, sel, inputs, varargin)
+defaults = {'latency', 0};
+latency = get_var('latency', 'defaults', defaults, varargin{:});
 
 if ~iscell(inputs)
     error('inputs must be a cell array')
@@ -8,4 +10,4 @@ selected = xSignal();
 config.source = 'Mux';
 config.name = name;
 
-xBlock(config, {'inputs', length(inputs)}, [{sel}, inputs], {selected});
+xBlock(config, {'inputs', length(inputs), 'latency', latency}, [{sel}, inputs], {selected});
