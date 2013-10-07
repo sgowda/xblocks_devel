7e8cbff3da8a62084c5dd58427745113f7d84733
Suraj Gowda <surajgowda@berkeley.edu>
2013-10-06 21:58:33 -0700

    new arch

--- xblocks_library/kurtosis_acc_bank_init_xblock.m
+++ xblocks_library/kurtosis_acc_bank_init_xblock.m
@@ -1,20 +1,34 @@
 function [] = kurtosis_acc_bank_init_xblock(blk, varargin)
 %% Config
-defaults = {'bit_width', 18, 'bin_pt', 17, 'acc_len', 2^14};
+defaults = {'bit_width', 18, 'bin_pt', 17, 'acc_len', 2^14, 'n_inputs', 1};
 bit_width = get_var('bit_width', 'defaults', defaults, varargin{:});
 bin_pt = get_var('bin_pt', 'defaults', defaults, varargin{:});
 acc_len = get_var('acc_len', 'defaults', defaults, varargin{:});
+n_inputs = get_var('n_inputs', 'defaults', defaults, varargin{:});
 
 %% Inports
 sync = xInport('sync');
-x_re = xInport('x_re');
-x_im = xInport('x_im');
-x_sq_re = xInport('x_sq_re');
-x_sq_im = xInport('x_sq_im');
-abs_x_sq = xInport('|x|^2');
-abs_x_4th = xInport('|x|^4');
-x_3rd_re = xInport('x_3rd_re');
-x_3rd_im = xInport('x_3rd_im');
+if n_inputs > 1
+    for k = 1:n_inputs
+        x_re{k} = xInport(sprintf('x_re_%d', k));
+        x_im{k} = xInport(sprintf('x_im_%d', k));
+        x_sq_re{k} = xInport(sprintf('x_sq_re_%d', k));
+        x_sq_im{k} = xInport(sprintf('x_sq_im_%d', k));
+        abs_x_sq{k} = xInport(sprintf('abs_x_sq_%d', k));
+        abs_x_4th{k} = xInport(sprintf('abs_x_4_%d', k));
+        x_3rd_re{k} = xInport(sprintf('x_3rd_re_%d', k));
+        x_3rd_im{k} = xInport(sprintf('x_3rd_im_%d', k));    
+    end
+else
+    x_re = xInport('x_re');
+    x_im = xInport('x_im');
+    x_sq_re = xInport('x_sq_re');
+    x_sq_im = xInport('x_sq_im');
+    abs_x_sq = xInport('|x|^2');
+    abs_x_4th = xInport('|x|^4');
+    x_3rd_re = xInport('x_3rd_re');
+    x_3rd_im = xInport('x_3rd_im');
+end
 
 %% Outports
 sync_out       = xOutport('sync_out');
@@ -26,6 +40,8 @@ abs_x_sq_mean  = xOutport('E[|x|^2]');
 abs_x_4th_mean = xOutport('E[|x|^4]');
 x_3rd_re_mean  = xOutport('Re{E[x|x|^2]}');
 x_3rd_im_mean  = xOutport('Im{E[x|x|^2]}');
+channel        = xOutport('channel');
+
 
 %% Diagram
 x_re_acc      = xSignal();
@@ -42,13 +58,14 @@ double_type_x = type_x^2 + type_x^2;
 triple_type_x = type_x * double_type_x;
 quad_type_x = double_type_x^2;
 
-single_vacc_params = {'veclen', 0, 'n_inputs', 1, 'max_accum', log2(acc_len), 'arith_type', 1, 'in_bit_width', type_x.WordLength, 'in_bin_pt', type_x.FractionLength, 'use_dsp48', 1, 'serialize_output_streams', 1};
-double_vacc_params = {'veclen', 0, 'n_inputs', 1, 'max_accum', log2(acc_len), 'arith_type', 1, 'in_bit_width', double_type_x.WordLength, 'in_bin_pt', double_type_x.FractionLength, 'use_dsp48', 1, 'serialize_output_streams', 1};
-triple_vacc_params = {'veclen', 0, 'n_inputs', 1, 'max_accum', log2(acc_len), 'arith_type', 1, 'in_bit_width', triple_type_x.WordLength, 'in_bin_pt', triple_type_x.FractionLength, 'use_dsp48', 1, 'serialize_output_streams', 1};
-quad_vacc_params = {'veclen', 0, 'n_inputs', 1, 'max_accum', log2(acc_len), 'arith_type', 1, 'in_bit_width', quad_type_x.WordLength, 'in_bin_pt', quad_type_x.FractionLength, 'use_dsp48', 1, 'serialize_output_streams', 1};
+single_vacc_params = {'veclen', 0, 'n_inputs', n_inputs, 'max_accum', log2(acc_len), 'arith_type', 1, 'in_bit_width', type_x.WordLength, 'in_bin_pt', type_x.FractionLength, 'use_dsp48', 1, 'serialize_output_streams', 1};
+double_vacc_params = {'veclen', 0, 'n_inputs', n_inputs, 'max_accum', log2(acc_len), 'arith_type', 1, 'in_bit_width', double_type_x.WordLength, 'in_bin_pt', double_type_x.FractionLength, 'use_dsp48', 1, 'serialize_output_streams', 1};
+triple_vacc_params = {'veclen', 0, 'n_inputs', n_inputs, 'max_accum', log2(acc_len), 'arith_type', 1, 'in_bit_width', triple_type_x.WordLength, 'in_bin_pt', triple_type_x.FractionLength, 'use_dsp48', 1, 'serialize_output_streams', 1};
+quad_vacc_params = {'veclen', 0, 'n_inputs', n_inputs, 'max_accum', log2(acc_len), 'arith_type', 1, 'in_bit_width', quad_type_x.WordLength, 'in_bin_pt', quad_type_x.FractionLength, 'use_dsp48', 1, 'serialize_output_streams', 1};
 
 acc_len_sig = const('acc_len', acc_len-1, fi_dtype(0,32,0));
 
+channel_0 = xSignal();
 x_re_acc_0 = xSignal();
 x_im_acc_0 = xSignal();
 x_sq_re_acc_0 = xSignal();
@@ -57,25 +74,26 @@ abs_x_sq_acc_0 = xSignal();
 sync_out_48bit = xSignal();
 
 % E[x] accumulator
-xBlock(struct('name', 'x_re_acc', 'source', @vacc_init_xblock), {[blk, '/x_re_acc'], single_vacc_params{:}}, {sync, acc_len_sig, x_re}, {sync_out_48bit, x_re_acc_0});
-xBlock(struct('name', 'x_im_acc', 'source', @vacc_init_xblock), {[blk, '/x_im_acc'], single_vacc_params{:}}, {sync, acc_len_sig, x_im}, {[], x_im_acc_0});
+xBlock(struct('name', 'x_re_acc', 'source', @vacc_init_xblock), {[blk, '/x_re_acc'], single_vacc_params{:}}, {sync, acc_len_sig, x_re{:}}, {sync_out_48bit, x_re_acc_0, channel_0});
+xBlock(struct('name', 'x_im_acc', 'source', @vacc_init_xblock), {[blk, '/x_im_acc'], single_vacc_params{:}}, {sync, acc_len_sig, x_im{:}}, {[], x_im_acc_0});
 
 % E[x^2] accumulator
-xBlock(struct('name', 'x_sq_re_acc', 'source', @vacc_init_xblock), {[blk, '/x_sq_re_acc'], double_vacc_params{:}}, {sync, acc_len_sig, x_sq_re}, {[], x_sq_re_acc_0});
-xBlock(struct('name', 'x_sq_im_acc', 'source', @vacc_init_xblock), {[blk, '/x_sq_im_acc'], double_vacc_params{:}}, {sync, acc_len_sig, x_sq_im}, {[], x_sq_im_acc_0});
+xBlock(struct('name', 'x_sq_re_acc', 'source', @vacc_init_xblock), {[blk, '/x_sq_re_acc'], double_vacc_params{:}}, {sync, acc_len_sig, x_sq_re{:}}, {[], x_sq_re_acc_0});
+xBlock(struct('name', 'x_sq_im_acc', 'source', @vacc_init_xblock), {[blk, '/x_sq_im_acc'], double_vacc_params{:}}, {sync, acc_len_sig, x_sq_im{:}}, {[], x_sq_im_acc_0});
 
 % E[x|x|^2] accmulator
-xBlock(struct('name', 'x_3rd_re_acc', 'source', @vacc_init_xblock), {[blk, '/x_3rd_re_acc'], triple_vacc_params{:}}, {sync, acc_len_sig, x_3rd_re}, {[], x_3rd_re_acc});
-xBlock(struct('name', 'x_3rd_im_acc', 'source', @vacc_init_xblock), {[blk, '/x_3rd_im_acc'], triple_vacc_params{:}}, {sync, acc_len_sig, x_3rd_im}, {[], x_3rd_im_acc});
+xBlock(struct('name', 'x_3rd_re_acc', 'source', @vacc_init_xblock), {[blk, '/x_3rd_re_acc'], triple_vacc_params{:}}, {sync, acc_len_sig, x_3rd_re{:}}, {[], x_3rd_re_acc});
+xBlock(struct('name', 'x_3rd_im_acc', 'source', @vacc_init_xblock), {[blk, '/x_3rd_im_acc'], triple_vacc_params{:}}, {sync, acc_len_sig, x_3rd_im{:}}, {[], x_3rd_im_acc});
 
 % E[|x|^2] accmulator
-xBlock(struct('name', 'abs_x_sq_acc', 'source', @vacc_init_xblock), {[blk, '/abs_x_sq_acc'], double_vacc_params{:}}, {sync, acc_len_sig, abs_x_sq}, {[], abs_x_sq_acc_0});
+xBlock(struct('name', 'abs_x_sq_acc', 'source', @vacc_init_xblock), {[blk, '/abs_x_sq_acc'], double_vacc_params{:}}, {sync, acc_len_sig, abs_x_sq{:}}, {[], abs_x_sq_acc_0});
 
 % E[|x|^4] accmulator
-xBlock(struct('name', 'abs_x_4th_acc', 'source', @vacc_init_xblock), {[blk, '/abs_x_4th_acc'], quad_vacc_params{:}}, {sync, acc_len_sig, abs_x_4th}, {[], abs_x_4th_acc});
+xBlock(struct('name', 'abs_x_4th_acc', 'source', @vacc_init_xblock), {[blk, '/abs_x_4th_acc'], quad_vacc_params{:}}, {sync, acc_len_sig, abs_x_4th{:}}, {[], abs_x_4th_acc});
 
 % match latencies between 96-bit accumulators and 48-bit
 sync_out.bind(delay_srl('', sync_out_48bit, 1));
+channel.bind(delay_srl('', channel_0, 1));
 
 x_re_acc     = delay_srl('', x_re_acc_0, 1);
 x_im_acc     = delay_srl('', x_im_acc_0, 1);
